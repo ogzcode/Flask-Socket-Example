a6cb85979ca98085634a5a8df7958f72a19421bf
ogzCode <oguzguc44@gmail.com>
2024-04-05 12:43:25 +0300

    Add User model to app/models.py

---
+++ app/models.py
@@ -0,0 +1,11 @@
+from app import db
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    email = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+    
+    def __repr__(self):
+        return f"<User {self.username}>"
