97de44c7aa60e27d5b7d7c279dc0b9215ee974fa
ogzCode <oguzguc44@gmail.com>
2024-04-15 17:32:05 +0300

    Add UserMixin to User model and create Message model in app/models.py

--- app/models.py
+++ app/models.py
@@ -1,11 +1,23 @@
+from flask_login import UserMixin
 from app import db
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
     email = db.Column(db.String(64), unique=True, index=True)
-    password_hash = db.Column(db.String(128))
+    password = db.Column(db.String(128))
     
     def __repr__(self):
         return f"<User {self.username}>"
+    
+class Message(db.Model):
+    __tablename__ = 'message'
+    id = db.Column(db.Integer, primary_key=True)
+    message = db.Column(db.String(128))
+    sender_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    receiver_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    
+    def __repr__(self):
+        return f"<Message {self.message}>"
