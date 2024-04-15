8639b31aee254a692f8725b7854fe414f0be497b
ogzCode <oguzguc44@gmail.com>
2024-04-15 17:33:04 +0300

    Add Flask-SocketIO for real-time communication

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config
 from flask_login import LoginManager
+from flask_socketio import SocketIO
 
 login_manager = LoginManager()
+login_manager.login_view = "login"
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+socketio = SocketIO(app)
 
 login_manager.init_app(app)
 
@@ -16,3 +20,7 @@ migrate = Migrate(app, db)
 
 
 from app import models, routes
+
+
+if __name__ == "__main__":
+    socketio.run(app)
