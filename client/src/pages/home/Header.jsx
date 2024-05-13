0cd76d458f853bee273299b095f7f42f0ae493b8
ogzCode <oguzguc44@gmail.com>
2024-05-13 17:35:33 +0300

    chore: Update Header component design and logout button styling

--- client/src/pages/home/Header.jsx
+++ client/src/pages/home/Header.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import chat from "../../assets/chat.png"
 
+import { BsBoxArrowRight } from "react-icons/bs";
+
 import { removeToken, removeAuthenticatedUser, getAuthenticatedUser } from '../../services/tokenServices'
 
 export const Header = () => {
@@ -12,17 +14,18 @@ export const Header = () => {
     }
 
     return (
-        <header className="col-span-3 px-8 bg-white shadow-sm rounded-md flex justify-between items-center h-16">
-            <div className='flex items-center gap-4'>
-                <img src={chat} alt="chat" className="w-10 h-10" />
-                <h1 className='text-2xl font-light text-slate-700 tracking-wider'><span className='text-teal-600 font-bold'>F</span>Chat</h1>
+        <div className="px-6 py-4 bg-white shadow-sm rounded-md border-t flex justify-between items-center">
+            <div className='flex items-center gap-2'>
+                <img src={chat} alt="chat" className="w-6 h-6" />
+                <h1 className='text-lg font-light text-slate-700 tracking-wider'><span className='text-teal-600 font-bold'>F</span>Chat</h1>
             </div>
             <div className="header-right">
-                <span className='bg-teal-100 text-teal-500 px-4 py-2 rounded-md font-semibold mr-4'>{authUser.username}</span>
                 <button
-                    className="bg-red-500 text-white px-4 py-2 rounded-md"
-                    onClick={handleLogout}>Logout</button>
+                    className="bg-red-500 text-white p-2 rounded-md hover:bg-red-600 transition"
+                    onClick={handleLogout}>
+                    <BsBoxArrowRight className='text-xl' />
+                </button>
             </div>
-        </header>
+        </div>
     )
 }
---
+++ client/src/pages/home/components/EmptyChat.jsx
@@ -0,0 +1,21 @@
+import chat1 from "../../../assets/image/chat-1.svg"
+import personelText from "../../../assets/image/personal-text.svg"
+
+export default function EmptyChat({ emptyType }) {
+    if (emptyType === 'main') {
+        return (
+            <div className="flex-1 flex justify-center items-center flex-col">
+                <img src={chat1} alt="chat" className="w-1/2 h-1/2" />
+                <span className="text-lg text-slate-600 mt-8">Select a user to start chat</span>
+            </div>
+        )
+    }
+    else if (emptyType === 'withUser') {
+        return (
+            <div className="flex justify-center items-center h-full flex-col">
+                <img src={personelText} alt="chat" className="w-1/2 h-1/2" />
+                <span className="text-slate-600 mt-8">No messages yet</span>
+            </div>
+        )
+    }
+}
