af4f84fc642ce1991332bf5f5b012dfece15da20
ogzCode <oguzguc44@gmail.com>
2024-05-13 17:35:43 +0300

    feat: Update Home component to fetch and display users upon connection

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,13 +1,16 @@
 import { useEffect, useState } from "react"
 import { socket } from "../../services/socket";
 
+import { getAuthenticatedUser } from "../../services/tokenServices";
+
 import { UserSide } from "./UserSide";
-import { Header } from "./Header";
 import { ChatSide } from "./ChatSide";
 
 export const Home = () => {
+    const authUser = getAuthenticatedUser()
     const [selectedUser, setSelectedUser] = useState(null)
     const [isConnected, setIsConnected] = useState(socket.connected)
+    const [roomId, setRoomId] = useState(null)
 
     useEffect(() => {
 
@@ -16,9 +19,11 @@ export const Home = () => {
         socket.on('connect', () => {
             setIsConnected(true)
             console.log('connected')
+            socket.emit("get_users", { user_id: authUser.id })
         })
         socket.on('disconnect', () => {
             console.log('disconnected')
+            socket.emit('leave', { disconnected_id: authUser.id })
             setIsConnected(false)
         })
 
@@ -30,10 +35,9 @@ export const Home = () => {
 
     return (
         <div
-            className="grid grid-cols-3 gap-8 p-12">
-            <Header />
-            <UserSide selectedUser={selectedUser} onChangeSelectedUser={setSelectedUser} />
-            <ChatSide selectedUser={selectedUser} />
+            className="min-h-screen grid grid-cols-3 gap-8 p-12">
+            <UserSide selectedUser={selectedUser} onChangeSelectedUser={setSelectedUser} roomId={roomId}/>
+            <ChatSide selectedUser={selectedUser} roomId={roomId} onChangeRoomId={setRoomId} />
         </div>
     )
 }
