0d08a76008e135d5ba23f4765de1d76ea8c8105d
ogzCode <oguzguc44@gmail.com>
2024-05-20 10:13:23 +0300

    feat: Refactor ChatSide component to improve message handling and user interaction

--- client/src/pages/home/ChatSide.jsx
+++ client/src/pages/home/ChatSide.jsx
@@ -1,15 +1,18 @@
 import React, { useState, useEffect, useRef } from 'react'
 import { getAuthenticatedUser } from '../../services/tokenServices'
 import { socket } from '../../services/socket'
-import { formatTime } from '../../utils/util';
 
 import EmptyChat from './components/EmptyChat';
+import MessageInput from './components/MessageInput';
+import MessageSeparator from './components/MessageSeparator';
+import Message from './components/Message';
+import MessageHeader from './components/MessageHeader';
+import { useChatStore } from '../../store/useChatStore';
 
-
-export const ChatSide = ({ selectedUser, roomId, onChangeRoomId }) => {
+export const ChatSide = () => {
     const authUser = getAuthenticatedUser()
-    const [message, setMessage] = useState('')
     const [messageList, setMessageList] = useState([]);
+    const { selectedUser, updateRoomId } = useChatStore()
 
     const messagesEndRef = useRef(null)
 
@@ -25,85 +28,52 @@ export const ChatSide = ({ selectedUser, roomId, onChangeRoomId }) => {
     useEffect(() => {
         socket.on('join', (data) => {
             setMessageList(data.messages)
-            onChangeRoomId(data.room_id)
+            updateRoomId(data.room_id)
         })
 
         socket.on('message', (data) => {
             setMessageList(prev => [...prev, data.message])
-            console.log("Message Data " + data.message)
-            
-            if (data.message.sender_id !== authUser.id) {
-                /* socket.emit('notification', {
-                    receiver_id: data.message.sender_id,
-                    message: 'New Message from ' + authUser.username
-                }) */
-                socket.emit("mark_as_read", { room_id: data.room_id, message_id: data.message.id})
-            }
-        })
 
-        socket.on("notification", (data) => {
-            console.log("Notification Data " + data.message)
+            if (data.message.sender_id !== authUser.id) {
+                socket.emit("mark_as_read", { room_id: data.room_id, message_id: data.message.id })
+            }
         })
 
         return () => {
             socket.off('join')
             socket.off('message')
-            socket.off('notification')
         }
     }, [])
 
-    const handleSendMessage = () => {
-        if (!message) return
-
-        socket.emit('message', {
-            receiver_id: selectedUser.id,
-            sender_id: authUser.id,
-            message: message,
-            room_id: roomId
-        })
-
-        setMessage('')
-
-    }
-
 
     return (
-        <div className="col-span-2 flex flex-col border rounded-xl border-slate-200 bg-white"
+        <div className="col-span-3 flex flex-col border rounded-xl border-slate-200 bg-white"
             style={{ height: 'calc(100vh - 6rem)', position: 'relative' }}
         >
             {
                 selectedUser ? (
                     <>
+                        <MessageHeader />
                         <div className="h-full px-4 pt-4 font-medium tracking-wide overflow-y-scroll flex-grow">
-                            {messageList.map((msg, index) => (
-                                <div key={index} className={`mb-6 flex flex-col ${msg.sender_id === authUser?.id ? 'justify-end' : 'justify-start'}`}>
-                                    <div>
-                                        <div className={`flex items-end gap-2 mb-2 ${msg.sender_id === authUser?.id ? 'flex-row-reverse' : ''}`}>
-                                            <span className="text-xs font-light text-slate-600">{formatTime(msg.created_at)}</span>
-                                        </div>
-                                        <div className={`w-max relative rounded-md
-                                            ${msg.sender_id === authUser?.id ? 'bg-teal-500 float-end text-white' : 'text-slate-700 border border-teal-500'}`}
-                                        >
-                                            <span className='inline-block p-2 text-sm'>{msg.message}</span>
-                                            <span className={`message-triangle ${msg.sender_id === authUser?.id ? 'triangle-right' : 'triangle-left'}`}></span>
-                                        </div>
-                                    </div>
-                                </div>
-                            ))}
+                            {messageList.map((msg, index) => {
+                                return (
+                                    <React.Fragment key={msg.id}>
+                                        {index > 0 && <MessageSeparator prevDate={messageList[index - 1].created_at} currentDate={msg.created_at} />}
+
+                                        <Message
+                                            message={msg}
+                                            isUserMessage={msg.sender_id === authUser.id}
+                                        />
+                                    </React.Fragment>
+                                );
+                            })}
+
                             <div ref={messagesEndRef} />
                             {
                                 messageList.length === 0 && <EmptyChat emptyType="withUser" />
                             }
                         </div>
-                        <div className="flex gap-6 w-full p-4" style={{ position: "sticky", bottom: "" }}>
-                            <input
-                                type="text"
-                                className="h-full w-full border border-slate-400 text-slate-800 outline-none rounded px-2 py-2"
-                                value={message}
-                                onChange={e => setMessage(e.target.value)}
-                            />
-                            <button className="h-full bg-teal-500 text-white rounded px-6 font-semibold" onClick={() => handleSendMessage()}>Send</button>
-                        </div>
+                        <MessageInput />
                     </>
                 ) : (
                     <EmptyChat emptyType="main" />
--- client/src/pages/home/UserSide.jsx
+++ client/src/pages/home/UserSide.jsx
@@ -1,20 +1,21 @@
 import React, { useEffect, useState } from 'react'
-import { getContactedUser, getAllUsers } from '../../services/api/user'
-import { BsSearch, BsChatFill, BsPersonCircle } from "react-icons/bs";
-import { formatTime } from '../../utils/util';
+import { getAllUsers } from '../../services/api/user'
+import { BsPersonCircle } from "react-icons/bs";
 import { socket } from '../../services/socket';
 import { getAuthenticatedUser } from '../../services/tokenServices';
 import { Header } from './Header';
+import UserBox from './components/UserBox';
 
-export const UserSide = ({ selectedUser, onChangeSelectedUser, roomId }) => {
+import { useChatStore } from '../../store/useChatStore';
+
+export const UserSide = () => {
     const authUser = getAuthenticatedUser()
     const [users, setUsers] = useState(null)
-    const [tab, setTab] = useState('chats')
-    const [search, setSearch] = useState('')
+    const [search, setSearch] = useState('');
+    const { roomId, updateSelectedUser } = useChatStore()
 
     useEffect(() => {
         socket.on('get_users', (data) => {
-            console.log(data.users)
             setUsers(data.users)
         })
 
@@ -24,7 +25,7 @@ export const UserSide = ({ selectedUser, onChangeSelectedUser, roomId }) => {
     }, [])
 
     useEffect(() => {
-        if (search !== '' && tab === 'all') {
+        if (search !== '') {
             const fetchUser = async () => {
                 const res = await getAllUsers(search)
                 setUsers(res.data.users)
@@ -33,25 +34,21 @@ export const UserSide = ({ selectedUser, onChangeSelectedUser, roomId }) => {
         }
     }, [search]);
 
-    const getActiveTabStyle = (tabName) => {
-        return tab === tabName ? 'bg-teal-500 text-white' : 'text-teal-600'
-    }
 
     const handleJoin = (user) => {
+
         socket.emit('join', {
             receiver_id: user.id,
             sender_id: authUser.id,
             old_room_id: roomId
         });
 
-        if (tab === 'all') {
-            setTab('chats')
+        if (search !== "") {
+            socket.emit("get_users", { user_id: authUser.id })
             setSearch('')
-            setUsers(null)
         }
-        else {
-            onChangeSelectedUser(user)
-        }
+
+        updateSelectedUser(user)
     }
 
     return (
@@ -59,7 +56,7 @@ export const UserSide = ({ selectedUser, onChangeSelectedUser, roomId }) => {
             style={{ height: 'calc(100vh - 6rem)', position: 'relative' }}
         >
             <div className=''>
-                <div className='flex items-center p-3 gap-4'>
+                <div className='flex items-center p-4 gap-4'>
                     <div>
                         <BsPersonCircle className="text-4xl text-teal-600" />
                     </div>
@@ -68,71 +65,27 @@ export const UserSide = ({ selectedUser, onChangeSelectedUser, roomId }) => {
                         <p className="text-xs text-slate-500">{authUser?.email}</p>
                     </div>
                 </div>
-                <div className="flex">
-                    <button className={'flex-1 flex justify-center items-center py-2 gap-2 font-semibold border-b border-b-teal-500 ' + getActiveTabStyle("chats")} onClick={() => setTab('chats')}>
-                        <BsChatFill className="inline-block text-xl" />
-                        <span>Chats</span>
-                    </button>
-                    <button className={'flex-1 flex justify-center items-center overflow-y-scroll gap-2 font-semibold border-b border-b-teal-500 ' + getActiveTabStyle("all")}
-                        onClick={() => { setTab('all'); setUsers(null) }}>
-                        <BsSearch className="inline-block text-xl" />
-                        <span>All</span>
-                    </button>
+                <div className='px-4 pt-2 pb-6 border-b border-slate-200'>
+                    <input
+                        value={search}
+                        onChange={(e) => setSearch(e.target.value)}
+                        type="text"
+                        placeholder="Search..."
+                        className="border border-slate-400 text-slate-700 rounded-md px-4 py-2 w-full outline-none focus:border-teal-500 focus:outline-teal-300 outline-offset-1"
+                    />
                 </div>
             </div>
-            <div className="p-6 flex-grow overflow-y-auto">
-                {tab === 'all' && (
-                    <div className="">
-                        <input
-                            type="text"
-                            placeholder="Search..."
-                            value={search}
-                            onChange={e => setSearch(e.target.value)}
-                            className='border border-slate-400 rounded-md px-4 py-2 w-full mt-2 mb-6'
+            <div className="p-4 flex-grow overflow-y-auto">
+                <div className="">
+                    {users?.map((item, index) => (
+                        <UserBox
+                            key={index}
+                            item={item}
+                            onSelectUser={handleJoin}
+                            isSearch={search !== ""}
                         />
-                        <div className="search-results">
-                            {users && users?.map(item => (
-                                <div key={item?.id}
-                                    className='flex items-center gap-4 p-4 mb-6 cursor-pointer relative rounded-md border border-slate-200  hover:bg-slate-50'
-                                    onClick={() => handleJoin(item)}>
-                                    <BsPersonCircle className="inline-block text-4xl text-teal-600" />
-                                    <h3 className='text-slate-800 text-lg font-semibold'>{item?.username}</h3>
-                                </div>
-                            ))}
-                            {
-                                !users && (
-                                    <div className="flex justify-center items-center h-32">
-                                        <p className="text-slate-500">No users found</p>
-                                    </div>
-                                )
-                            }
-                        </div>
-                    </div>
-                )}
-                {tab === 'chats' && (
-                    <div className="">
-                        {users?.map(item => (
-                            <div key={item?.user?.id}
-                                className={`flex items-center gap-4 p-4 mb-6 cursor-pointer relative rounded-md border border-slate-200  hover:bg-slate-50 ${selectedUser?.id === item?.user?.id ? 'bg-slate-50' : ''}`}
-                                onClick={() => handleJoin(item?.user)}>
-                                <BsPersonCircle className="inline-block text-4xl text-teal-600" />
-                                <div>
-                                    <h3 className='text-slate-800 text-lg font-semibold'>{item?.user?.username}</h3>
-                                    <p className='text-sm text-slate-500'>{item?.last_message_date ? item?.last_message : "No message"}</p>
-                                </div>
-                                {item?.last_message && <p className='text-xs text-gray-500 absolute right-3 top-3'>{formatTime(item?.last_message_date)}</p>}
-                                {
-                                    item?.unread_messages_count > 0 && (
-                                        <div className='absolute right-3 bottom-3 bg-teal-500 text-white rounded-full w-6 h-6 flex justify-center items-center'>
-                                            <p className='text-xs'>{item?.unread_messages_count}</p>
-                                        </div>
-                                    )
-
-                                }
-                            </div>
-                        ))}
-                    </div>
-                )}
+                    ))}
+                </div>
             </div>
 
             <div className='sticky bottom-0'>
