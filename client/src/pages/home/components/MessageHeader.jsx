7b9e2d87e8c659bc22d05598984768af3b9986d2
ogzCode <oguzguc44@gmail.com>
2024-05-20 10:13:38 +0300

    refactor: Move deleteChat functionality to MessageHeader component

--- client/src/pages/home/components/MessageHeader.jsx
+++ client/src/pages/home/components/MessageHeader.jsx
@@ -1,23 +1,56 @@
+import { useState } from "react";
 import { TbUserHexagon } from "react-icons/tb";
-import DropdownMenu from "../../../components/DropdownMenu";
 import { BiBlock } from "react-icons/bi";
 import { RiDeleteBin2Fill } from "react-icons/ri";
 
-export default function MessageHeader({ selectedUser }) {
+import DropdownMenu from "../../../components/DropdownMenu";
+import ConfirmDialog from "../../../components/ConfirmDialog";
+
+import { deleteChat } from "../../../services/api/message";
+
+import { useChatStore } from "../../../store/useChatStore";
+import { getAuthenticatedUser } from "../../../services/tokenServices";
+import { useToast } from "../../../context/useToast";
+import { socket } from "../../../services/socket";
+
+export default function MessageHeader() {
+    const [open, setOpen] = useState(false);
+    const { selectedUser, roomId, updateRoomId, updateSelectedUser } = useChatStore();
+    const authUser = getAuthenticatedUser();
+    const { showToast } = useToast();
+    
+
     const items = [
-        {
-            text: "Delete",
-            onClickFunc: () => console.log("Delete"),
-            icon: () => <RiDeleteBin2Fill className="inline-block text-xl text-slate-700" />,
-            itemStyle: "text-red-500"
-        },
         {
             text: "Block",
             onClickFunc: () => console.log("Block"),
-            icon: () => <BiBlock className="inline-block text-xl text-slate-700" />,
-            itemStyle: "text-red-500"
+            icon: () => <BiBlock className="inline-block text-xl text-indigo-700" />,
+            itemStyle: "text-indigo-600 hover:bg-indigo-50"
+        },
+        {
+            text: "Delete Chat",
+            onClickFunc: () => setOpen(true),
+            icon: () => <RiDeleteBin2Fill className="inline-block text-xl text-red-500" />,
+            itemStyle: "text-red-600 hover:bg-red-50"
         }
     ]
+
+    const handleDeleteChat = async () => {
+        try {
+            socket.emit('leave', { disconnected_id: authUser.id })
+            await deleteChat(roomId);
+            socket.emit("get_users", { user_id: authUser.id })
+            updateRoomId(null);
+            updateSelectedUser(null);
+            showToast({ message: "Chat deleted successfully", type: "success" });
+        } catch (error) {
+            console.log(error);
+            showToast({ message: "Failed to delete chat", type: "error" });
+        }
+
+        setOpen(false);
+    }
+
     return (
         <div className="flex items-center justify-between px-4 py-2 border-b border-gray-200 shadow ">
             <div className="flex items-center gap-2">
@@ -28,6 +61,15 @@ export default function MessageHeader({ selectedUser }) {
                 </div>
             </div>
             <DropdownMenu items={items} />
+
+            <ConfirmDialog
+                open={open}
+                onClose={() => setOpen(false)}
+                onConfirm={handleDeleteChat}
+                title="Delete Chat"
+                message="Are you sure you want to delete this chat?"
+                severity="danger"
+            />
         </div>
     )
 }
