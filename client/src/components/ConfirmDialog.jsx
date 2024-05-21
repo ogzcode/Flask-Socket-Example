5f1881aa9e1a216ea352c41b431121bf5e732712
ogzCode <oguzguc44@gmail.com>
2024-05-21 17:59:29 +0300

    refactor: Improve ConfirmDialog component styling and add zIndex prop

--- client/src/components/ConfirmDialog.jsx
+++ client/src/components/ConfirmDialog.jsx
@@ -18,20 +18,20 @@ export default function ConfirmDialog({ open, onClose, onConfirm, title, message
         }
     }
     return (
-        <div className="fixed top-0 left-0 h-full w-full bg-stone-500/20 flex justify-center items-center z-50">
+        <div className="fixed top-0 left-0 h-full w-full bg-stone-500/20 flex justify-center items-center" style={{ zIndex: 1000 }}>
             <div className="bg-white p-6 rounded-lg shadow-md w-96">
-                <div className="flex justify-between items-center">
+                <div className="flex justify-between items-center" >
                     <h1 className="text-lg font-semibold text-stone-800">{title}</h1>
                     <button onClick={onClose}>
                         <FaXmark className='text-xl text-stone-700' />
                     </button>
-                </div>
+                </div >
                 <p className="text-stone-700 break-all mt-4">{message}</p>
                 <div className="flex justify-end gap-4 mt-4">
                     <button onClick={onClose} className="px-3 py-1 text-sm tracking-wide font-medium border border-stone-300 text-stone-700 rounded-md">Cancel</button>
                     <button onClick={onConfirm} className={`px-3 py-1 text-sm tracking-wide font-medium text-white rounded-md ${getStatusColor()}`}>Confirm</button>
                 </div>
-            </div>
+            </div >
         </div >
     )
 }
